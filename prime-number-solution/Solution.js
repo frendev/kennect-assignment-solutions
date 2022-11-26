b7c00a25b1d108c056f1d027e6d18106e4ba47d8
Adwait Gore <adwait.vmgore@gmail.com>
2022-11-26 19:28:13 +0530

    added js file of the code

---
+++ prime-number-solution/Solution.js
@@ -0,0 +1,28 @@
+const x = 5; //it's a prime number
+const isPrime = (x) => {
+  let sqrtnum = Math.floor(Math.sqrt(x));
+  let prime = true;
+  for (let i = 2; i < sqrtnum + 1; i++) {
+    if (x % i === 0) {
+      prime = false;
+      return false;
+    }
+  }
+  return true;
+};
+
+const nextPrime = (x) => {
+  while (!isPrime(++x)) {}
+  return x;
+};
+
+const primeNum = isPrime(x);
+
+if (primeNum) {
+  console.log(`The ${x} is a prime number.`);
+  const y = nextPrime(x);
+  const difference = y - x;
+  console.log(`The difference between ${y} and ${x} is ${difference}.`);
+} else {
+  console.log(`The ${x} is not a prime number.`);
+}
